102af4ddfe9649649ada37286ebc3b594827ad80
GDWR <gregory.dwr@gmail.com>
2023-02-26 19:21:29 +0000

    Add GitHub login

--- abandonauth/routes/__init__.py
+++ abandonauth/routes/__init__.py
@@ -1,7 +1,9 @@
-from .discord import router as register_router
+from .discord import router as discord_router
+from .github import router as github_router
 from .index import router as index_router
 
 routers = [
+    discord_router,
+    github_router,
     index_router,
-    register_router,
 ]
---
+++ abandonauth/routes/github.py
@@ -0,0 +1,63 @@
+import httpx
+from abandonauth.dependencies.auth.jwt import generate_jwt
+from abandonauth.models import JwtDto
+from abandonauth.settings import settings
+from fastapi import APIRouter
+from prisma.models import User
+from starlette.responses import RedirectResponse
+
+router = APIRouter(
+    prefix="/github",
+    tags=["GitHub"]
+)
+
+GITHUB_API_BASE = "https://github.com/login/oauth"
+
+
+@router.get("", response_model=JwtDto)
+async def login_with_github(code: str, state: str) -> RedirectResponse:
+    """Log a user in using GitHubs's OAuth2 as validation."""
+
+    data = {
+        "client_id": settings.GITHUB_CLIENT_ID,
+        "redirect_uri": settings.GITHUB_CALLBACK,
+        "client_secret": settings.GITHUB_CLIENT_SECRET.get_secret_value(),
+        "code": code,
+    }
+
+    headers = {"Accept": "application/json"}
+    async with httpx.AsyncClient(headers=headers) as client:
+        response = await client.post("https://github.com/login/oauth/access_token", data=data)
+        response.raise_for_status()
+
+        token = response.json()["access_token"]
+
+        response = await client.get(
+            "https://api.github.com/user",
+            headers={"Authorization": f"Bearer {token}"}
+        )
+
+        response.raise_for_status()
+        github_user = response.json()
+
+    user = await User.prisma().find_first(
+        where={
+            "github_account": {
+                "is": {
+                    "id": int(github_user["id"])
+                }
+            }
+        }
+    )
+
+    if user is None:
+        user = await User.prisma().create({
+            "username": github_user["login"],
+            "github_account": {
+                "create": {
+                    "id": int(github_user["id"])
+                }
+            }
+        })
+
+    return RedirectResponse(f"{state}?authentication={generate_jwt(user.id)}")
