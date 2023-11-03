4ecaa5c12594e7c8d8d69fdb58b34dad22ac9b78
Kyler Roloff <wakefollower@gmail.com>
2023-11-02 21:56:40 -0500

    Add Jwt models
    This is to help standardize JWT claims across the project and allow for better documentation for what to expect on a token claim and for the return type of the JWT dependency.

--- abandonauth/models/auth.py
+++ abandonauth/models/auth.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+from enum import Enum
+
 from pydantic import BaseModel
 
 
@@ -5,3 +8,22 @@ class JwtDto(BaseModel):
     """Contains jwt token data to be sent to a client."""
 
     token: str
+
+
+class ScopeEnum(str, Enum):
+    identify = "identify"
+
+
+class LifespanEnum(str, Enum):
+    long = "long"
+    short = "short"
+
+
+class JwtClaimsDataDto(BaseModel):
+    """All claim data for an Abandon Auth JWT"""
+
+    user_id: str
+    exp: datetime
+    scope: ScopeEnum
+    aud: str
+    lifespan: LifespanEnum
