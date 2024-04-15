f018876da5d042bc669fd291dea52c35dc051635
Kyler <39353605+fisher60@users.noreply.github.com>
2024-04-14 20:23:25 -0500

    create initial AbandonAuth Nuxt site (#80)

---
+++ site/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+    <footer id="footer" class="footer h-64 lg:h-full p-10 bg-primary text-primary-content">
+        <div class="flex flex-col items-center text-2xl gap-4 mx-auto">
+            <div class="flex flex-row gap-3">
+                <ClientOnly>
+                    <NuxtLink to="https://github.com/AbandonTech/abandonauth" target="_blank"><font-awesome-icon icon="fa-brands fa-github" /></NuxtLink>
+                    <NuxtLink to="mailto:info@abandontech.cloud" target="_blank"><font-awesome-icon icon="fa-solid fa-envelope" /></NuxtLink>
+                </ClientOnly>
+            </div>
+            <p>info@abandontech.cloud</p>
+        </div>
+    </footer>
+</template>
---
+++ site/pages/developer-applications/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Dev apps</h1>
+</template>
