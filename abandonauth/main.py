4637d61581c199549834112e6fc54b44bc1ed6a5
Fisher60 <wakefollower@gmail.com>
2024-06-15 17:21:30 -0500

    add cors config to abandon auth backend, configure cors rules for prod and local dev

--- abandonauth/main.py
+++ abandonauth/main.py
@@ -2,10 +2,12 @@ import tomllib
 from pathlib import Path
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 
 from abandonauth.database import prisma_db
 from abandonauth.routers import routers
+from abandonauth.settings import settings
 
 with Path("../pyproject.toml").open("rb") as f:
     pyproject = tomllib.load(f)
@@ -13,6 +15,19 @@ with Path("../pyproject.toml").open("rb") as f:
 app = FastAPI(
     title="AbandonAuth",
     version=pyproject["tool"]["poetry"]["version"],
+)
+
+allowed_origins = [settings.ABANDON_AUTH_SITE_URL]
+
+if settings.DEBUG:
+    allowed_origins.append("*")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allowed_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.mount("/static", StaticFiles(directory="./abandonauth/static"), name="static")
