a4ad361a2632ddacd1b35976ce9cebca623ddd82
GDWR <gregory.dwr@gmail.com>
2025-03-25 17:56:37 +0000

    Remove python implementation and relocate website

--- src/api/prisma/migrations/20240421170756_add_autoincrement_for_id_in_password_account/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-CREATE SEQUENCE passwordaccount_id_seq;
-ALTER TABLE "PasswordAccount" ALTER COLUMN "id" SET DEFAULT nextval('passwordaccount_id_seq');
-ALTER SEQUENCE passwordaccount_id_seq OWNED BY "PasswordAccount"."id";
