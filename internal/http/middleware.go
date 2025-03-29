d2c330f19596409827635b44bb0b1397650d5924
GDWR <gregory.dwr@gmail.com>
2025-03-29 15:41:25 +0000

    Do not log query as we need sanitization to do so

--- internal/http/middleware.go
+++ internal/http/middleware.go
@@ -14,7 +14,6 @@ func LoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Debug().
 			Str("method", r.Method).
-			Str("query", r.URL.RawQuery).
 			Str("path", r.URL.Path).
 			Str("remote", r.RemoteAddr).
 			Send()
