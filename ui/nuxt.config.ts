90489306ddacc909fdce662283206ccec22f8aab
GDWR <gregory.dwr@gmail.com>
2025-03-30 12:02:33 +0100

    Set nuxt compatibilityDate

--- ui/nuxt.config.ts
+++ ui/nuxt.config.ts
@@ -5,22 +5,27 @@ export default defineNuxtConfig({
       '/api': process.env.ABANDON_AUTH_URL,
     },
   },
+
   devtools: {
     enabled: true,
     timeline: {
       enabled: true
     }
   },
+
   css: ["/assets/css/main.css", "@fortawesome/fontawesome-svg-core/styles.css"],
   modules: ["@nuxtjs/tailwindcss"],
+
   tailwindcss: {
     exposeConfig: true,
   },
+
   app: {
     head: {
       title: "AbandonAuth",
     }
   },
+
   runtimeConfig: {
     public: {
         abandonAuthUrl: process.env.ABANDON_AUTH_URL,
@@ -29,5 +34,7 @@ export default defineNuxtConfig({
         discordRedirect: process.env.DISCORD_REDIRECT,
         loginPath: `/login?application_id=${process.env.ABANDON_AUTH_DEVELOPER_APP_ID}&callback_uri=${process.env.ABANDON_AUTH_URL}/api/ui`
     }
-  }
+  },
+
+  compatibilityDate: "2025-03-29"
 })
